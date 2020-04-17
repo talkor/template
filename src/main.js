2c7a0375f3d1c7762f1c8e79ac879f5f5144113e
talkor <talkor@users.noreply.github.com>
2020-04-17 22:03:02 +0300

    basic changes for template

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import VueCompositionApi from '@vue/composition-api';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
